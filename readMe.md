07f1c0efe9dcc86a96763ce73aa0f7792464d3c3
monkey <monkey@lwork.com>
2016-11-17 18:28:02 +0800

    jsonwebtoken

--- readMe.md
+++ readMe.md
@@ -1,3 +1,11 @@
 ### 基于node的后台自动化开发 ###
 调试参考资料：http://xzper.com/2015/11/29/%E6%90%AD%E5%BB%BAvscode%E8%B0%83%E8%AF%95%E7%8E%AF%E5%A2%83/
 参考资料：https://segmentfault.com/a/1190000007305516
+
+<!--API接口设计-->
+参考资料:http://yoyoyohamapi.me/2015/07/10/Web/RMM%20Level%20--%20%E5%AF%B9%E4%BA%8EREST%E7%9A%84%E5%B1%82%E7%BA%A7%E5%88%92%E5%88%86%E6%A8%A1%E5%9E%8B/
+Result{status,msg,data}
+msgcode: 状态码
+msgtext: 状态描述
+[recnum: 记录(文档)数量]
+data:返回对应的数据
--- src/routers/root.ts
+++ src/routers/root.ts
@@ -1,9 +1,16 @@
 import { AppDataConfig } from './getConfigData'
 import * as express from 'express'
+import * as jsonWebpToken from 'jsonwebtoken'
 
 var router = express.Router()
 router.get('/', (req: express.Request, res: express.Response, next: any) => {
     let appData = new AppDataConfig()
+    var token = jsonWebpToken.sign({ 'name': 'monkey', 'age': 12 }, 'monkeyskey')
+    console.log('token--->', token)
+    var decode = jsonWebpToken.verify(token, 'monkeyskey')
+    console.log('decode--->', decode)
+    var plain = jsonWebpToken.decode(token)
+    console.log('plain--->', plain)
     appData.client.connect(appData.serverUrl, (err: any, db: any) => {
         if (err) {
             res.sendStatus(500)
@@ -15,7 +22,6 @@ router.get('/', (req: express.Request, res: express.Response, next: any) => {
                 res.sendStatus(404)
                 throw err
             }
-            console.log(result)
             res.json(result)
         })
     })
