89929aa3d7fddc4eb57be8f2de6b300de5a41c76
monkey <monkey@lwork.com>
2016-11-18 18:29:29 +0800

    异常处理

--- src/routers/root.ts
+++ src/routers/root.ts
@@ -4,22 +4,32 @@ import * as jsonWebpToken from 'jsonwebtoken'
 
 var router = express.Router()
 router.get('/', (req: express.Request, res: express.Response, next: any) => {
+    //throw new Error('abc')
     let appData = new AppDataConfig()
-    var token = jsonWebpToken.sign({ 'name': 'monkey', 'age': 12 }, 'monkeyskey')
+    var token = jsonWebpToken.sign({ 'name': 'monkey', 'age': 12, 'exp': Math.floor(Date.now() / 1000 + 0) }, 'monkeyskey')
     console.log('token--->', token)
-    var decode = jsonWebpToken.verify(token, 'monkeyskey')
-    console.log('decode--->', decode)
     var plain = jsonWebpToken.decode(token)
     console.log('plain--->', plain)
+    try {
+        var decode = jsonWebpToken.verify(token, 'monkeyskey')
+        console.log('decode--->', decode)
+    }
+    catch (err) {
+        // console.log(err.message)
+        // 此处应该是业务错误，不应该影响系统的运行
+        throw err
+    }
+
     appData.client.connect(appData.serverUrl, (err: any, db: any) => {
         if (err) {
+            console.log('conntion error', err)
             res.sendStatus(500)
-            throw err
+            throw new Error(err.message)
         }
-        console.log(appData.serverUrl)
         db.collection('users').find().toArray((err: any, result: any) => {
             if (err) {
                 res.sendStatus(404)
+                console.log('query error', err)
                 throw err
             }
             res.json(result)
